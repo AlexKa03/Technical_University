d3660c15246aa6f43f65eed642c03facb6c05d0b
Alex Kadiyski <65862735+AlexKa03@users.noreply.github.com>
2023-03-03 01:40:16 +0200

    Uploaded files

---
+++ 1. Introduction to Programming/01. Exam Prep/Exercise 1/Exercise 1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercise_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Switch the values betwen a and b
+
+            Console.Write("Enter number a: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter number b: ");
+            int b = int.Parse(Console.ReadLine());
+
+            int temp = a;
+            a = b;
+            b = temp;
+
+            Console.WriteLine($"a = {a}, b = {b}");
+        }
+    }
+}
