b282adc341fcae3d630db12954a1a3aa26beedeb
Alex Kadiyski <65862735+AlexKa03@users.noreply.github.com>
2023-03-03 01:42:06 +0200

    Uploaded files

---
+++ 1. Introduction to Programming/01. Exam Prep/Exercise 5/Exercise 5.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercise_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+               Write an application that finds the sum of the first N number of targets
+               positive numbers. N is a positive integer that is introduced by
+               the user. 
+            */
+
+            Console.Write("Please enter positive integer: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                sum += i;
+            }
+
+            Console.WriteLine($"Sum of the number 1 to {n} is {sum}");
+        }
+    }
+}
