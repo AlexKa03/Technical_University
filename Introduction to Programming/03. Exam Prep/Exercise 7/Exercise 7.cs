f682b29f6d5bd2d9b508334beaa99be6b4bf3909
Alex Kadiyski <65862735+AlexKa03@users.noreply.github.com>
2023-03-03 01:01:29 +0200

    Uploaded files

---
+++ Introduction to Programming/03. Exam Prep/Exercise 7/Exercise 7.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Exercise_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Write a function that finds the smallest
+                value of a one- dimensional integer array.
+            */
+
+            Console.Write($"How long will be this one-dimensional array: ");
+            int n = int.Parse(Console.ReadLine());
+            int[] array = new int[n];
+
+            InputArray(array);
+            int smallestValue = FindSmallestValue(array);
+            PrintSmallestValue(smallestValue);
+        }
+
+        static void InputArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"Enter value for element [{i}]: ");
+
+                int value = int.Parse(Console.ReadLine());
+                array[i] = value;
+            }
+        }
+
+        static int FindSmallestValue(int[] array)
+        {
+            int min = int.MaxValue;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] < min)
+                {
+                    min = array[i];
+                }
+            }
+
+            return min;
+        }
+
+        static void PrintSmallestValue(int smallestValue)
+        {
+            Console.WriteLine($"Smallest value of the array is {smallestValue}.");
+        }
+
+    }
+}
---
+++ Introduction to Programming/03. Exam Prep/Exercise 8/Exercise 8.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Exercise_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+               Write a function that finds the smallest
+               value from each row of a two-dimensional
+               integer array / matrix. 
+            */
+
+            Console.Write($"How long will be the row of this matrix: ");
+            int row = int.Parse(Console.ReadLine());
+
+            Console.Write($"How long will be the column of this matrix: ");
+            int col = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[row, col];
+
+            InputArray(matrix);
+            int[] allSmallestValues = FindSmallestValueForRows(matrix);
+            PrintAllSmallestValues(allSmallestValues);
+        }
+
+        static void InputArray(int[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write($"Enter a value for this cordinate [{row}, {col}]: ");
+
+                    int value = int.Parse(Console.ReadLine());
+                    matrix[row, col] = value;
+                }
+            }
+        }
+
+        static int[] FindSmallestValueForRows(int[,] matrix)
+        {
+            int[] allSmallestValues = new int[matrix.GetLength(0)];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int smallestValue = int.MaxValue;
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (matrix[row, col] < smallestValue)
+                    {
+                        smallestValue = matrix[row, col];
+                    }
+                }
+
+                allSmallestValues[row] = smallestValue;
+            }
+
+            return allSmallestValues;
+        }
+
+        static void PrintAllSmallestValues(int[] allSmallestValues)
+        {
+            for (int i = 0; i < allSmallestValues.Length; i++)
+            {
+                Console.WriteLine($"Smallest value at line {i} is {allSmallestValues[i]}.");
+            }
+        }
+    }
+}
