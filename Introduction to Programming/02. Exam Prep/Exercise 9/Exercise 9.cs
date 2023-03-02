78145cee6a42d484ceab0ec37e250213bba349b8
Alex Kadiyski <65862735+AlexKa03@users.noreply.github.com>
2023-03-03 00:27:29 +0200

    Uploaded files

---
+++ Introduction to Programming/02. Exam Prep/Exercise 9/Exercise 9.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Exercise_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Read two integer numbers. Sum them
+                and print them. Every action needs
+                to have it's function
+
+                Hint:
+                SumAndPrint
+                Sum
+                PrintResult
+            */
+
+            Console.WriteLine("Exercise 9");
+
+            Console.Write("What is your first number: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("What is your second number: ");
+            int b = int.Parse(Console.ReadLine());
+
+            SumAndPrint(a, b);
+        }
+
+        static void SumAndPrint(int a, int b)
+        {
+            int result = Sum(a, b);
+            PrintResult(result);
+        }
+
+        static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        static void PrintResult(int result) 
+        {
+            Console.WriteLine($"Result is {result}.");
+        }
+    }
+}
