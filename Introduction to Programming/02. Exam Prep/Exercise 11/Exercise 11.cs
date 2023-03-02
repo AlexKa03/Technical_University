be881aa51602ac9b143f16ab0adb8a6e79383afb
Alex Kadiyski <65862735+AlexKa03@users.noreply.github.com>
2023-03-03 00:28:57 +0200

    Uploaded files

---
+++ Introduction to Programming/02. Exam Prep/Exercise 11/Exercise 11.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Exercise_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Write a program with recursive function
+                named "PowerR", which calculate the value
+                of number m, raised to the power of n. Call
+                the function with example parameters.
+            */
+
+
+            Console.WriteLine("Exercise 11");
+
+            double result = PowerR(3, 4);
+            Console.WriteLine($"Result: {result}");
+        }
+
+        static int PowerR(int m, int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+
+            return m * PowerR(m, n - 1);
+        }
+    }
+}
