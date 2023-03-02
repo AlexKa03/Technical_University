1e9180447606be26880e3d647f22a65190f79d08
Alex Kadiyski <65862735+AlexKa03@users.noreply.github.com>
2023-03-03 00:26:03 +0200

    Uploaded files

---
+++ Introduction to Programming/02. Exam Prep/Exercise 6/Exercise 6.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Exercise_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+               Print first and last name.
+            */
+
+            Console.WriteLine("Exercise 6");
+
+            Console.Write("What is your first name: ");
+            string firstName = Console.ReadLine();
+
+            Console.Write("What is your last name: ");
+            string lastName = Console.ReadLine();
+
+            printMyName(firstName, lastName);
+
+            // It's void function that is why we prin inside of it, not here.
+        }
+
+        static void printMyName(string firstName, string lastName) 
+        {
+            string name = String.Format($"My name is {firstName} {lastName}.");
+            Console.WriteLine(name);
+        }
+    }
+}
