09fb82a62d6e74c3be6f9e86306242dc31555ec8
Alex Kadiyski <65862735+AlexKa03@users.noreply.github.com>
2023-03-03 02:13:32 +0200

    Uploaded files

---
+++ 1. Introduction to Programming/03. Exam Prep/Exercise 10/Exercise 10.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Exercise_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+               A function that reverses the elements of an
+               array,and returns a new array without changing
+               the original array. Inputs are not given by
+               the user.
+            */
+
+            int[] array = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int[] reversedArray = ReversedArray(array);
+
+            PrintArray(array);
+            Console.WriteLine();
+            PrintArray(reversedArray);
+        }
+
+        static int[] ReversedArray(int[] array) 
+        {
+            int n = array.Length;
+            int[] reversed = new int[n];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                reversed[i] = array[n - 1 - i];
+            }
+
+            return reversed;
+        }
+
+        static void PrintArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+        }
+    }
+}
---
+++ 1. Introduction to Programming/03. Exam Prep/Exercise 11/Exercise 11.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Exercise_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+               A function that reverses the elements of a given array
+               without creating a new one (modifies the original array).
+            */
+
+            int[] array = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            PrintArray(array);
+
+            ReversedInSameArray(array);
+            
+            Console.WriteLine();
+            PrintArray(array);
+        }
+
+        static void ReversedInSameArray(int[] array)
+        {
+            int n = array.Length;
+            for (int i = 0; i < n / 2; i++)
+            {
+                int tmp = array[i];
+                array[i] = array[n - i - 1];
+                array[n - i - 1] = tmp;
+            }
+        }
+
+        static void PrintArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+        }
+    }
+}
