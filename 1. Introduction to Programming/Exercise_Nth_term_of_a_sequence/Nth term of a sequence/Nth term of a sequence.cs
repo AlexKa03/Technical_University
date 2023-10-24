656cb72ab929aedb703a01b1053e375196f9f635
Alex Kadiyski <alex_kadiyski2003@abv.bg>
2023-10-24 19:40:15 +0300

    1. Introduction to Programming
    1. Introduction to Programming

---
+++ 1. Introduction to Programming/Exercise_Nth_term_of_a_sequence/Nth term of a sequence/Nth term of a sequence.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Nth_term_of_a_sequence
+{
+    internal class Program
+    {
+        public static int CalculatingElement(int numIndex, int[] array)
+        {
+            for (int i = 3; i <= numIndex - 1; i++)
+            {
+                array[i] = (3 * array[i - 3]) + (4 * array[i - 2]) - (7 * array[i - 1]);
+            }
+
+            return array[numIndex - 1];
+        }
+
+        public static int CalculateRecursive(int firstNum, int secondNum, int thirdNum, int numIndex)
+        {
+            int searchedNum;
+            searchedNum = (3 * firstNum) + (4 * secondNum) - (7 * thirdNum);
+            numIndex--;
+
+            if (numIndex > 3)
+
+            {
+                return CalculateRecursive(secondNum, thirdNum, searchedNum, numIndex);
+            }
+
+            return searchedNum;
+
+        }
+
+        static void Main(string[] args)
+        {
+            int numIndex = int.Parse(Console.ReadLine());
+            int[] recurrentArray = new int[100];
+
+            recurrentArray[0] = 2;
+            recurrentArray[1] = 4;
+            recurrentArray[2] = 6;
+
+            Console.WriteLine(CalculatingElement(numIndex, recurrentArray));
+            Console.WriteLine(CalculateRecursive(2, 4, 6, numIndex));
+        }
+    }
+}
